6ef1aaa7ba235f1659a0a5d935b68e07ce016afb
siddkhade3 <siddkhade7@gmail.com>
2025-09-14 18:35:52 +0530

    add simon game files

---
+++ app.js
@@ -0,0 +1,88 @@
+let gameSeq = [];
+let userSeq = [];
+
+let btns = ["yellow","red","purple","green"];
+let started = false;
+let level = 0;
+
+//Start game.
+let h3 = document.querySelector("h3");
+document.addEventListener("keypress",function(){
+    if(started == false){
+        console.log("Game started");
+        started = true;
+        levelUP();
+    }
+   
+});
+
+//Flash buttons and levelUP
+
+function gameFlash(btn){
+    btn.classList.add("flash");
+    setTimeout(function(){
+    btn.classList.remove("flash");
+    },300);
+}
+function userFlash(btn){
+    btn.classList.add("userFlash");
+    setTimeout(function(){
+    btn.classList.remove("userFlash");
+    },300);
+}
+
+function levelUP(){
+    userSeq =[];
+    level ++;
+    h3.innerText = `Level ${level}`;
+
+    let randIdx = Math.floor(Math.random()*4);
+    let randColor = btns[randIdx];
+    let randBtn = document.querySelector(`.${randColor}`);
+    gameSeq.push(randColor);
+    console.log(gameSeq);
+    gameFlash(randBtn);
+}
+
+// Matching Sequence
+
+function checkAns(idx){
+ 
+  if(userSeq[idx]===gameSeq[idx]){
+    if(userSeq.length == gameSeq.length){
+        setTimeout(levelUP,1000)
+    }
+  }else{
+      h3.innerHTML = `Game Over! | <i>Your Score - ${level}</i> | Press any key to start.`;
+      document.querySelector("body").style.backgroundColor = "red";
+      setTimeout(function(){
+        document.querySelector("body").style.backgroundColor = "white";
+      },150);
+      reset();
+  }
+}
+//Button Event listeners
+
+function btnPress(){
+   let btn = this;
+//    console.log(btn);
+   userFlash(btn);
+   userColor = btn.getAttribute("id");
+   userSeq.push(userColor)
+//    console.log(userColor);
+   checkAns(userSeq.length-1);
+}
+
+let allBtns = document.querySelectorAll(".btn");
+for(btn of allBtns){
+    btn.addEventListener("click",btnPress);
+}
+
+//Reset game
+
+function reset(){
+    started = false;
+    gameSeq = [];
+    userSeq = [];
+    level = 0;
+}
